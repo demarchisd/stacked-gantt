40997ee0978287ccbc973044c813f0faee14bca4
Bruno Kewitz Demarchi <TECBMBKD@TECLOGICA.COM.BR>
2018-05-08 15:06:08 -0300

    Generating v0.2.0 dist files

--- dist/jquery.stacked-gantt.js
+++ dist/jquery.stacked-gantt.js
@@ -81,6 +81,8 @@
 		$this.zoomOut = function() { stackedGantt.zoomOut(); };
 		$this.destroy = function() { stackedGantt.destroy(); };
 		$this.getData = function() { return stackedGantt.getData(); };
+		$this.getGeneralMarkers = function() { return stackedGantt.getGeneralMarkers(); };
+		$this.getGeneralHighlights = function() { return stackedGantt.getGeneralHighlights(); };
 		
  		return $this;
  	};
@@ -241,6 +243,14 @@
 
 		this.getData = function() {
 			return data;
+		};
+
+		this.getGeneralMarkers = function() {
+			return generalMarkers;
+		};
+
+		this.getGeneralHighlights = function() {
+			return generalHighlights;
 		};
 
  		function sanitizeDataDates(data)
